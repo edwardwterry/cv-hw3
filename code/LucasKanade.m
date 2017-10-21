e9b2c873fbd1040681f92eab9da6438bbb7e95b5
Edward Terry <edward.william.terry@gmail.com>
2017-10-21 14:33:50 -0400

    Q1.3 works

--- code/LucasKanade.m
+++ code/LucasKanade.m
@@ -4,18 +4,17 @@ function [dp_x,dp_y] = LucasKanade(It, It1, rect)
 % output - movement vector, [dp_x, dp_y] in the x- and y-directions.
 
 rect = uint16(rect); % converting to integers so it doesn't complain
-T = It(rect(2):rect(4),rect(1):rect(3)); % create a template based on It
+T = It(rect(2):rect(4),rect(1):rect(3)); % create a template based on It. Updates every frame
 
 [x,y,~]=size(It);
 [xt,yt,~]=size(T);
 
 p = [0 0]'; % initial guess for p
-delta_p = [0 0]'; % initial guess for p
 
 eps = 1e-3; % convergence threshold
 [It1x,It1y] = gradient(It1);
 
-[V,U] = meshgrid(1:y,1:x); % x and y coordinates of rectangle
+[U,V] = meshgrid(1:y,1:x); % x and y coordinates of rectangle
 c=cat(2,U',V');
 coords=double([reshape(c,[],2) ones(size(U,1)*size(U,2),1)]);
 
@@ -23,18 +22,17 @@ i = 1;
 metric = 1;
 metric_difference = 1;
 
-while metric_difference(i)>eps%% && i<50
+while metric_difference>eps%% && i<50
     
     % warp I with W(x,p) to compute I(W(x,p))
-    W = [1 0 delta_p(1); 0 1 delta_p(2)];
-    warped_coords = (W*coords')'; % 
-    Uq = reshape(warped_coords(:,2),[y,x])'; % make into equivalent meshgrids to above
-    Vq = reshape(warped_coords(:,1),[y,x])';
+    W = [1 0 p(1); 0 1 p(2)];
+    warped_coords = (W*coords')'; %
+    Vq = reshape(warped_coords(:,2),[y,x])'; % make into equivalent meshgrids to above
+    Uq = reshape(warped_coords(:,1),[y,x])';
     I_w = interp2(It1,Uq,Vq); % warped It1
     I_w_crop = I_w(rect(2):rect(4),rect(1):rect(3));
-
+    
     % compute error image
-%     b = It-I_w;
     bc = T-I_w_crop;
     bc_col = reshape(bc,[xt*yt,1]);
     
@@ -43,31 +41,32 @@ while metric_difference(i)>eps%% && i<50
     delIx_w_crop = delIx_w(rect(2):rect(4),rect(1):rect(3));
     delIy_w = interp2(It1y,Uq,Vq);
     delIy_w_crop = delIy_w(rect(2):rect(4),rect(1):rect(3));
-    delIcol = [reshape(delIx_w,[x*y,1]) reshape(delIy_w,[x*y,1])];
+    %     delIcol = [reshape(delIx_w,[x*y,1]) reshape(delIy_w,[x*y,1])];
     delIcol_crop = [reshape(delIx_w_crop,[xt*yt,1]) reshape(delIy_w_crop,[xt*yt,1])];
     
     % evaluate Jacobian at (x,p)
     J = [1 0;0 1];
     
     % compute steepest descent images: delI * Jacobian
-    A = delIcol_crop*J;    
-        Ax = reshape(A(:,1),[yt,xt])'; % turn back to a matrix
-        Ay = reshape(A(:,1),[yt,xt])'; % turn back to a matrix
-
+    A = delIcol_crop*J;
+    %     Ax = reshape(A(:,1),[yt,xt])'; % turn back to a matrix
+    %     Ay = reshape(A(:,1),[yt,xt])'; % turn back to a matrix
+    
     % compute Hessian
     H = A'*A;
     
     % compute delta_p
     delta_p = H\A'*bc_col;
-        
+    
     % update the parameters
     i = i+1;
-%     p = p+delta_p;    
+    p = p+delta_p;
     
     past_metric = metric;
     metric = (norm(delta_p))^2;
-    metric_difference(i) = abs(metric-past_metric);
+    metric_difference = abs(metric-past_metric);
 end
-dp_x = delta_p(1);
-dp_y = delta_p(2);
+
+dp_x = p(1);
+dp_y = p(2);
 end
--- code/testCarSequence.m
+++ code/testCarSequence.m
@@ -4,33 +4,23 @@ close all
 
 load('../data/carseq.mat');
 frames = im2double(frames);
-It = frames(:,:,200);
-It1 = frames(:,:,201);
+
 rect = [60,117,146,152]';
-% rect = [186,121,146-60+186,152-117+121]';
-rect_history(:,1) = rect;
-% h = waitbar(0,'Please wait...');
-% steps = size(frames,3);
-% for i = 2:size(frames,3)
-%     [dp_x,dp_y] = LucasKanade(frames(:,:,i-1), frames(:,:,i), rect_history(:,i-1));
-%     rect_history(:,i) = [rect_history(1,i-1)+dp_x,rect_history(2,i-1)+dp_y,rect_history(3,i-1)+dp_x,rect_history(4,i-1)+dp_y]';
-%     waitbar(i / steps)
-% end
-% for i = 2:15%%size(frames,3)
-    [dp_x,dp_y] = LucasKanade(It, It1, rect);
-%     rect_history(:,i) = [rect_history(1,i-1)+dp_x,rect_history(2,i-1)+dp_y,rect_history(3,i-1)+dp_x,rect_history(4,i-1)+dp_y]';            
+rects(:,1) = rect;
+
+for i = 2:size(frames,3)
+    [dp_x,dp_y] = LucasKanade(frames(:,:,i-1), frames(:,:,i), rects(:,i-1));
+    rects(:,i) = [rects(1,i-1)+dp_x,rects(2,i-1)+dp_y,rects(3,i-1)+dp_x,rects(4,i-1)+dp_y]';
+end
+
+% for i = 1:size(frames,3)
+%     imshow(frames(:,:,i));
+%     hold on
+%     rectangle('Position',[rects(1,i) rects(2,i) rect(3)-rect(1) rect(4)-rect(2)])
+%     hold off
+%     pause(0.001)
 % end
 
-% for i = 1:415%%size(frames,3)
-% imshow(frames(:,:,i));
-% hold on
-% rectangle('Position',[rect_history(1,i) rect_history(2,i) rect(3)-rect(1) rect(4)-rect(2)])
-% hold off
-% pause(0.01)
-% end
-% hold on
-% imshow(It)
-% rectangle('Position',[rect(1) rect(2) rect(3)-rect(1) rect(4)-rect(2)])
-% figure 
-% imshow(It1)
-% rectangle('Position',[rect(1)+dp_x rect(2)+dp_y rect(3)-rect(1) rect(4)-rect(2)])
+rects=rects';
+
+save('carseqrects.mat','rects')
